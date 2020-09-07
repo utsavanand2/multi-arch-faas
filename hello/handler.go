59f9007f166948cdb355fc3db9e8193644f189fb
Utsav Anand <utsavanand2@gmail.com>
2020-09-07 20:15:25 +0530

    change res message

--- hello/handler.go
+++ hello/handler.go
@@ -6,5 +6,5 @@ import (
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Hello from Utsav's OpenFaas function running on ARM")
+	return fmt.Sprintf("Hello from Utsav's OpenFaas function running on Raspberry Pi ARM")
 }
