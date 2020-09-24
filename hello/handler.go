068012ae1ac1db4063e2f55cf3626ed65de9a360
Utsav Anand <utsavanand2@gmail.com>
2020-09-25 00:40:00 +0530

    Update response message

--- hello/handler.go
+++ hello/handler.go
@@ -11,7 +11,7 @@ import (
 func Handle(req handler.Request) (handler.Response, error) {
 	var err error
 
-	message := fmt.Sprintf("Hello world, input was: %s", string(req.Body))
+	message := fmt.Sprintf("Hello Kartik")
 
 	return handler.Response{
 		Body:       []byte(message),
